d3efd76ef9eb1dccdb99583d2e2b3e44940b2889
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2025-10-30 12:25:16 +0200

    removed commented text, add skip for negative tests

--- src/api/AuthApiClient.ts
+++ src/api/AuthApiClient.ts
@@ -1,33 +1,7 @@
 import { BaseAPIClient } from "../core/BaseAPIClient";
 
-// type AdminSignupInvitesQuery = {
-//   sort?: string[];
-//   range?: (number | string)[];
-//   filter?: Record<string, string>;
-// };
-
 export class AuthApiClient extends BaseAPIClient {
-  async health() {
-    return this.get("/v1/health");
-  }
-
-  // async getAdminSignupInvites(customParams: AdminSignupInvitesQuery = {}) {
-  //   const params = Object.fromEntries(
-  //     Object.entries(customParams).map(([k, v]) => [k, JSON.stringify(v)])
-  //   );
-
-  //   return this.get("/v1/signup-invites", params);
-  // }
-
   async postSignupRequestStepOne(body: Record<string, any> = {}) {
     return this.post("/v1/auth/signup/request/step-one", body);
   }
-
-  // async patchSignupInvite(id: string | number){
-  //   return this.patch(`/v1/signup-invites/${id}/resend`);
-  // }
-
-  //   async deleteSignupInvite(id: string | number){
-  //   return this.delete(`/v1/signup-invites/${id}`);
-  // }
-} 
+}
