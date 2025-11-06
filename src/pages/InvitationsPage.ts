996bde0a73c601084430c7bb1b9bcf6eab785720
Oleg Georgiyevski <oleqg@yahoo.com>
2025-11-06 11:54:18 +0300

    fix ui tests

--- src/pages/InvitationsPage.ts
+++ src/pages/InvitationsPage.ts
@@ -1,5 +1,6 @@
 import { BasePage } from "../core/BasePage";
 import { Page, expect } from "@playwright/test";
+import { IUserData, MessageStatus } from '../data/invitationData';
 
 export class InvitationsPage extends BasePage {
   constructor(page: Page) {
@@ -18,51 +19,48 @@ export class InvitationsPage extends BasePage {
     await expect(this.page).toHaveURL(/#\/invitations\/create/);
   }
 
-  async expectMessage(message: string, timeout = 15000) {
-    const locator = this.page.locator("p", { hasText: message });
-    await locator.first().waitFor({ timeout });
-    const actualCount = await locator.count();
-    console.log(`Found ${actualCount} message(s) with text: "${message}"`);
-    await expect(locator.first()).toBeVisible();
+  async expectMessage(message: string, timeout = 15000){
+      const locator = this.page.locator("p", { hasText: message });
+      await locator.first().waitFor({ timeout });
+      const actualCount = await locator.count();
+      console.log(`Found ${actualCount} message(s) with text: "${message}"`);
+      await expect(locator.first()).toBeVisible();
   }
 
-  async fillAndSend(
-    first: string,
-    last: string,
-    email: string,
-    company: string,
-    message: string
-  ) {
-    await this.page.getByLabel(/first name/i).fill(first);
-    await this.page.getByLabel(/last name/i).fill(last);
-    await this.page
-      .locator('input[type="text"][name="companyName"]')
-      .fill(company);
-    await this.page.locator('input[type="text"][name="email"]').fill(email);
-    await this.page.getByRole("button", { name: /send invitation/i }).click();
-    await this.expectMessage(message);
+  async fillAndSend(userData: IUserData){
+      const { firstName, lastName, email, company } = userData;
+
+      await this.page.getByLabel(/first name/i).fill(firstName);
+      await this.page.getByLabel(/last name/i).fill(lastName);
+      await this.page
+          .locator('input[type="text"][name="companyName"]')
+          .fill(company);
+      await this.page.locator('input[type="text"][name="email"]').fill(email);
+      await this.page.getByRole("button", { name: /send invitation/i }).click();
   }
 
   async sendWithEmptyFields() {
-    await this.page.getByRole("button", { name: /send invitation/i }).click();
-    const fields = [
-      { name: "First name", error: "First name is required" },
-      { name: "Last name", error: "Last name is required" },
-      { name: "Company name", error: "Company name is required" },
-      { name: "Email", error: "Email is required" },
-    ];
+      await this.page.getByRole("button", { name: /send invitation/i }).click();
+  }
 
-    for (const { name, error } of fields) {
-      const input = this.page.getByRole("textbox", { name });
-      await expect(input, `Expect ${name} to be red color`).toHaveCSS(
-        "color",
-        "rgb(211, 47, 47)"
-      );
-      const errorMessage = this.page.getByText(error);
-      await expect(
-        errorMessage,
-        `Expected error message: ${error} is not visible`
-      ).toBeVisible();
-    }
+  async expectValidationErrors() {
+      const fields = [
+          { name: "First name", error: "First name is required" },
+          { name: "Last name", error: "Last name is required" },
+          { name: "Company name", error: "Company name is required" },
+          { name: "Email", error: "Email is required" },
+      ];
+
+      for (const { name, error } of fields) {
+          const input = this.page.getByRole("textbox", { name });
+          await expect(input, `Expect ${name} to be red color`).toHaveCSS(
+              "color",
+              "rgb(211, 47, 47)"
+          );
+          await expect(
+              this.page.getByText(error),
+              `Expected error message: ${error} is not visible`
+          ).toBeVisible();
+      }
   }
 }
--- tests/ui/admin-send-invitation.spec.ts
+++ tests/ui/admin-send-invitation.spec.ts
@@ -1,70 +1,65 @@
 import { test } from "@playwright/test";
 import { InvitationsPage } from "../../src/pages/InvitationsPage";
 import { ENV } from "../../src/config/env";
-import { Invitations } from "../../src/data/invitationData";
+import { Invitations, Messages } from "../../src/data/invitationData";
 import { createStorageState } from "../../src/core/createStorageState";
 
 test.describe("ADA-US-001: Admin sends invitation", () => {
-  let adminStoragePath = "storage/admin.json";
-  let inv: InvitationsPage;
+    let adminStoragePath = "storage/admin.json";
+    let inv: InvitationsPage;
 
-  test.beforeAll(async ({ browser }) => {
-    await createStorageState(
-      browser,
-      ENV.admin.email,
-      ENV.admin.password,
-      adminStoragePath
-    );
-    const context = await browser.newContext({
-      storageState: adminStoragePath,
+    test.beforeAll(async ({ browser }) => {
+        await createStorageState(
+            browser,
+            ENV.admin.email,
+            ENV.admin.password,
+            adminStoragePath
+        );
+        const context = await browser.newContext({
+            storageState: adminStoragePath,
+        });
+        const page = await context.newPage();
+        inv = new InvitationsPage(page);
     });
-    const page = await context.newPage();
-    inv = new InvitationsPage(page);
-  });
 
-  test("should show success toaster after sending Buyer invitations", async () => {
-    await inv.open();
-    await inv.openCreateForm();
-    await inv.fillAndSend(
-      Invitations.buyer.firstName,
-      Invitations.buyer.lastName,
-      ENV.invites.buyerEmail,
-      Invitations.buyer.company,
-      Invitations.messages.success
-    );
-  });
+    test("should show success toaster after sending Buyer invitations", async () => {
+        await inv.open();
+        await inv.openCreateForm();
+        const buyerData = Invitations.buyer;
+        //Act
+        await inv.fillAndSend(buyerData);
+        //Assert
+        await inv.expectMessage(Messages.success)
+    });
 
-  test("should show success toaster after sending Vendor invitations", async () => {
-    await inv.open();
-    await inv.openCreateForm();
-    await inv.fillAndSend(
-      Invitations.vendor.firstName,
-      Invitations.vendor.lastName,
-      ENV.invites.vendorEmail,
-      Invitations.vendor.company,
-      Invitations.messages.success
-    );
-  });
+    test("should show success toaster after sending Vendor invitations", async () => {
+        await inv.open();
+        await inv.openCreateForm();
+        const vendorData = Invitations.vendor;
+        //Act
+        await inv.fillAndSend(vendorData);
+        //Assert
+        await inv.expectMessage(Messages.success);
+    });
 
-  test("should show validation error when send Buyer invitations with the same email", async () => {
-    await inv.open();
-    await inv.openCreateForm();
-    await inv.fillAndSend(
-      Invitations.buyer.firstName,
-      Invitations.buyer.lastName,
-      Invitations.buyer.email,
-      Invitations.buyer.company,
-      Invitations.messages.duplicate
-    );
-  });
+    test("should show validation error when send Buyer invitations with the same email", async () => {
+        await inv.open();
+        await inv.openCreateForm();
+        const buyerData = Invitations.buyer;
+        //Act
+        await inv.fillAndSend(buyerData);
+        //Assert
+        await inv.expectMessage(Messages.duplicate);
+    });
 
-  test("should show validation errors when clicking Invitation button without filling data", async () => {
-    await inv.open();
-    await inv.openCreateForm();
-    await inv.sendWithEmptyFields();
-  });
+    test("should show validation errors when clicking Invitation button without filling data", async () => {
+        await inv.open();
+        await inv.openCreateForm();
+        await inv.sendWithEmptyFields();
+        await inv.expectValidationErrors();
+    });
 
-  test.afterAll(async () => {
-    await inv.close();
-  });
+    test.afterAll(async () => {
+        await inv.close();
+    });
 });
