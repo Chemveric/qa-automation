784ccdd5cf23c82f8dfcfbd0ab9a8fc85f8b88d5
Oleg Georgiyevski <oleqg@yahoo.com>
2025-11-19 00:12:03 +0300

    update

--- tests/api/organizations/editOrganizationDetail.spec.ts
+++ tests/api/organizations/editOrganizationDetail.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from "@playwright/test";
 import { ResponseValidationHelper } from "../../../helpers/ResponseValidationHelper";
+import { randomUUID } from "crypto";
 import {
   getAdminCookie,
   getBuyerCookie,
   getSupplierCookie,
 } from "../../../src/utils/getEnv";
 import { OrganizationSchema } from "../../../src/schema/organizationSchema";
-import { OrganizationsApiClient } from "../../../src/api/OrganizationsApiClient";
-import { DictionarySupportedCountriesSchema } from "../../../src/schema/dictionarySupportedCountriesSchema";
-import { DictionaryApiClient } from "../../../src/api/DictionaryApiClient";
-import { ENV } from "../../../src/config/env";
-import { organizationTestData } from "../../../src/utils/invalidData/invalidEditOrganization";
 import { validateResponse } from "../../../helpers/schemaResponseValidator";
-import { faker } from "@faker-js/faker";
+import { OrganizationsApiClient } from "../../../src/api/OrganizationsApiClient";
+import { ENV } from "../../../src/config/env";
+import { DictionaryApiClient } from "../../../src/api/DictionaryApiClient";
+import { DictionarySupportedCountriesSchema } from "../../../src/schema/dictionarySupportedCountriesSchema";
+import { organizationTestData } from "../../../src/utils/invalidData/invalidEditOrganization";
 
 const validator = new ResponseValidationHelper();
 
@@ -23,6 +23,7 @@ test.describe("API: edit organization details", () => {
   const api = new OrganizationsApiClient();
   let countries: Record<string, any>[];
   let buyerId: string;
+  let supplierId: string;
 
   test.beforeAll(async () => {
     buyerCookie = getBuyerCookie();
@@ -30,12 +31,11 @@ test.describe("API: edit organization details", () => {
     adminCoockie = getAdminCookie();
     await api.init({}, buyerCookie);
     const res = await api.getOrganizationDetails();
-    expect(
-      res.status,
-      `Expected status code is 200, but got ${res.status}`
-    ).toBe(200);
     const body = await res.body;
     buyerId = body.id;
+    const supplierRes = await api.getOrganizationDetails();
+    const supplierBody = await supplierRes.body;
+    supplierId = supplierBody.id;
     const dictionaryApi = new DictionaryApiClient();
     await dictionaryApi.init({ "Content-Type": false });
     const countryRes = await dictionaryApi.getDictionarySupportedCountries({});
@@ -47,7 +47,8 @@ test.describe("API: edit organization details", () => {
     );
   });
 
-  test(`edit organization with valid data`, async () => {
+  test(`edit organization with valid data for buyer`, async () => {
+    await api.init({}, buyerCookie);
     const randomCountry =
       countries[Math.floor(Math.random() * countries.length)];
     const countryId = randomCountry.id;
@@ -57,20 +58,18 @@ test.describe("API: edit organization details", () => {
       regionId,
       countryId,
       licenseFilesIds: [],
-      name: faker.company.name(),
-      state: faker.location.state(),
-      city: faker.location.city(),
-      street: faker.location.streetAddress(),
-      postalCode: faker.location.zipCode(),
-      email: faker.internet.email(),
+      name: "Some name",
+      state: "California",
+      city: "Some city",
+      street: "Test street",
+      postalCode: "54345",
+      email: ENV.buyer.email,
     };
     const editRes = await api.editOrganizationDetail(params);
     expect(
       editRes.status,
       `Expected status code is 200, but got ${editRes.status}`
     ).toBe(200);
-
-    await api.init({}, buyerCookie);
     const res = await api.getOrganizationDetails();
     expect(
       res.status,
@@ -94,8 +93,56 @@ test.describe("API: edit organization details", () => {
     expect(validated.email).toBe(ENV.buyer.email);
   });
 
+  test(`edit organization with valid data for supplier`, async () => {
+    await api.init({}, supplierCookie);
+    const randomCountry =
+      countries[Math.floor(Math.random() * countries.length)];
+    const countryId = randomCountry.id;
+    const regionId = randomCountry.regionId;
+    const params = {
+      id: buyerId,
+      regionId,
+      countryId,
+      licenseFilesIds: [],
+      name: "Some name",
+      state: "California",
+      city: "Some city",
+      street: "Test street",
+      postalCode: "54345",
+      email: ENV.vendor.email,
+    };
+    const editRes = await api.editOrganizationDetail(params);
+    expect(
+      editRes.status,
+      `Expected status code is 200, but got ${editRes.status}`
+    ).toBe(200);
+
+    const res = await api.getOrganizationDetails();
+    expect(
+      res.status,
+      `Expected status code is 200, but got ${res.status}`
+    ).toBe(200);
+
+    const body = await res.body;
+    console.log("Response status:", res.status);
+    console.log("Response body:", JSON.stringify(res.body, null, 2));
+    const validated = await validateResponse(
+      { status: res.status, body },
+      OrganizationSchema
+    );
+    expect(validated.countryId).toBe(params.countryId);
+    expect(validated.regionId).toBe(params.regionId);
+    expect(validated.name).toBe(params.name);
+    expect(validated.state).toBe(params.state);
+    expect(validated.city).toBe(params.city);
+    expect(validated.street).toBe(params.street);
+    expect(validated.postalCode).toBe(params.postalCode);
+    expect(validated.email).toBe(ENV.vendor.email);
+  });
+
   for (const testCase of organizationTestData) {
     test(testCase.name, async () => {
+      await api.init({}, buyerCookie);
       const randomCountry =
         countries[Math.floor(Math.random() * countries.length)];
       const { params, expectedMessage } = testCase.getData(
@@ -110,17 +157,19 @@ test.describe("API: edit organization details", () => {
 
   test("should return 401 Unauthorized when login with admin cookie", async () => {
     await api.init({}, adminCoockie);
+    const randomCountry =
+      countries[Math.floor(Math.random() * countries.length)];
     const params = {
       id: buyerId,
-      regionId: faker.string.uuid(),
-      countryId: faker.string.uuid(),
+      regionId: randomCountry.regionId,
+      countryId: randomCountry.id,
       licenseFilesIds: [],
-      name: faker.company.name(),
-      state: faker.location.state(),
-      city: faker.location.city(),
-      street: faker.location.streetAddress(),
-      postalCode: faker.location.zipCode(),
-      email: faker.internet.email(),
+      name: "Some company",
+      state: "California",
+      city: "Some street",
+      street: "Some street",
+      postalCode: "32453",
+      email: "semeEmail@company.com",
     };
     const res = await api.editOrganizationDetail(params);
     validator.expectStatusCodeAndMessage(res, 401, "Unauthorized");
