00a29d6b3a1b3c0632479acc73f0463938368cf0
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2025-11-19 17:36:56 +0200

    post/publish rfq tests

--- src/api/RfqsApiClient.ts
+++ src/api/RfqsApiClient.ts
@@ -1,12 +1,15 @@
 import { BaseAPIClient } from "../core/BaseAPIClient";
 
 export class RfqsApiClient extends BaseAPIClient {
-  async postRfqs(body: Record<string, any> = {}) { 
+  async postRfqs(body: Record<string, any> = {}) {
     return this.post("/v1/rfqs", body);
   }
 
-  async getRfqById(id: string){
+  async getRfqById(id: string) {
     return this.get(`/v1/rfqs/${id}`);
   }
+
+  async postRfqsIdPublish(id: string, body: Record<string, any> = {}) {
+    return this.post(`/v1/rfqs/${id}/publish`, body);
+  }
 }
- 
