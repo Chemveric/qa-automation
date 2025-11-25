56621ee36aaf377e975d731c972cbe0be547ffff
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2025-11-25 21:13:49 +0200

    use storage state in team management tests

--- tests/ui/company-members.spec.ts
+++ tests/ui/company-members.spec.ts
@@ -1,53 +1,41 @@
-import { test } from "../../src/base/testFixtures.ui";
-import { UserLoginPage } from "../../src/pages/UserLoginPage";
-import { UserDashboardPage } from "../../src/pages/UserDashboardPage";
-import { UserMainProductsPage } from "../../src/pages/UserMainProductsPage";
+import { CookiesTag, DriverProvider } from "../../src/driver/DriverProvider";
 import { UserMembersPage } from "../../src/pages/UserMembersPage";
-import { ENV } from "../../src/config/env";
-import { da } from "@faker-js/faker/.";
+import { test } from "@playwright/test";
 
 test.describe("Team Managemant", () => {
+  let teamMembersPage: UserMembersPage;
+  test.use({
+    storageState: DriverProvider.getCookiesStateFileName(CookiesTag.Buyer),
+  });
+
+  test.beforeEach(async ({ page }) => {
+    teamMembersPage = new UserMembersPage(page);
+  });
   test("buyer should successfully invite team member", async ({ page }) => {
-    const buyer = new UserLoginPage(page);
-    await buyer.loginWithAuth0(ENV.buyer.email, ENV.buyer.password);
-
-    // // navigate on Dashboard page
-    // const mainPage = new UserMainProductsPage(page);
-    // await mainPage.sidebar.openDashboard();
-
-    // // open Dashbord page
-    // const dashboardPage = new UserDashboardPage(page);
-    // await dashboardPage.assertLoaded();
-    // // await page.pause();
-    // await dashboardPage.clickOnTeamManagement();
-
-    const teamMembersPage = new UserMembersPage(page);
     await teamMembersPage.goto();
     await teamMembersPage.assertLoaded();
 
+    // act
     await teamMembersPage.clickOnInviteMember();
     await teamMembersPage.fillMemberFirstName();
     await teamMembersPage.fillMemberLastName();
     await teamMembersPage.fillMemberEmail();
     await teamMembersPage.assignRandomRole();
-    // await page.pause();
     await teamMembersPage.saveChanges();
 
     //assert
     await teamMembersPage.assertSuccessMessageIsVisible();
   });
 
-  test("buyer should successfully invite admin member", async ({ page }) => {
-    const buyer = new UserLoginPage(page);
-    await buyer.loginWithAuth0(ENV.buyer.email, ENV.buyer.password);
-
-    const teamMembersPage = new UserMembersPage(page);
+  test("buyer should successfully invite admin member: Bug", async ({
+    page,
+  }) => {
     await teamMembersPage.goto();
     await teamMembersPage.assertLoaded();
 
+    // act
     await teamMembersPage.clickOnInviteMember();
     await teamMembersPage.checkAdminUserRadioButton();
-    // await page.pause();
     await teamMembersPage.fillMemberFirstName();
     await teamMembersPage.fillMemberLastName();
     await teamMembersPage.fillMemberEmail();
@@ -57,18 +45,14 @@ test.describe("Team Managemant", () => {
     await teamMembersPage.assertSuccessMessageIsVisible();
   });
 
-  test("should show validation errors when invite member without filled in data", async ({ page }) => {
-    const buyer = new UserLoginPage(page);
-    await buyer.loginWithAuth0(ENV.buyer.email, ENV.buyer.password);
-
-    const teamMembersPage = new UserMembersPage(page);
+  test("should show validation errors when invite member without filled in data", async ({
+    page,
+  }) => {
     await teamMembersPage.goto();
     await teamMembersPage.assertLoaded();
 
+    // act
     await teamMembersPage.clickOnInviteMember();
-
-    await page.pause();
-
     await teamMembersPage.saveChanges();
 
     //assert
